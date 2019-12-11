e232be09fec441b2ae07cfba60c6c780933c5758
NguyenNguyen <nguyenbk92@gmail.com>
2019-12-11 17:33:16 +0700

    tomoxService and tomoxlending service share the same database

--- tomoxDAO/interfaces.go
+++ tomoxDAO/interfaces.go
@@ -1,13 +1,16 @@
-package tomoxlending
+package tomoxDAO
 
 import (
 	"github.com/globalsign/mgo"
 	"github.com/tomochain/tomochain/common"
 	"github.com/tomochain/tomochain/ethdb"
+	"github.com/tomochain/tomochain/tomox/tomox_state"
 	"github.com/tomochain/tomochain/tomoxlending/lendingstate"
 )
 
-type OrderDao interface {
+const defaultCacheLimit = 1024
+
+type TomoXDAO interface {
 	// for both leveldb and mongodb
 	IsEmptyKey(key []byte) bool
 	Close()
@@ -17,11 +20,20 @@ type OrderDao interface {
 	GetObject(hash common.Hash, val interface{}) (interface{}, error)
 	PutObject(hash common.Hash, val interface{}) error
 	DeleteObject(hash common.Hash, val interface{}) error // won't return error if key not found
-	GetOrderByTxHash(txhash common.Hash) []*lendingstate.LendingItem
-	GetListOrderByHashes(hashes []string) []*lendingstate.LendingItem
-	DeleteTradeByTxHash(txhash common.Hash)
-	InitBulk() *mgo.Session
-	CommitBulk() error
+
+		// basic tomox
+		InitBulk() *mgo.Session
+		CommitBulk() error
+		GetOrderByTxHash(txhash common.Hash) []*tomox_state.OrderItem
+		GetListOrderByHashes(hashes []string) []*tomox_state.OrderItem
+		DeleteTradeByTxHash(txhash common.Hash)
+
+		// tomox lending
+		InitLendingBulk() *mgo.Session
+		CommitLendingBulk() error
+		GetLendingItemByTxHash(txhash common.Hash) []*lendingstate.LendingItem
+		GetListLendingItemByHashes(hashes []string) []*lendingstate.LendingItem
+		DeleteLendingTradeByTxHash(txhash common.Hash)
 
 	// leveldb methods
 	Put(key []byte, value []byte) error
